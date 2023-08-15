265612cef61dcdb2ee0c2b3e66e1e5d1419f2a10
Richard <richardsanvie@gmail.com>
2023-08-15 09:33:24 -0300

    update Commmit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+
+function comprimeImage(){
+    return gulp.src('./source/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'));
+}
+
+function comprimeJavaScript(){
+    return gulp.src('./source/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/scripts'))
+}
+
+function compilaSass(){
+    return gulp.src('./source/styles/main.scss')
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe(gulp.dest('./build/styles'));
+}
+
+exports.sass = compilaSass;
+exports.javaScript = comprimeJavaScript;
+exports.images = comprimeImage;
